bd248e3addfa7678d3adf9c9924cd6f754f21f11
luvi-kh <nalsengom@daum.net>
2024-07-06 17:02:24 +0900

    environment, applicationContext방식으로 property 가져옴

--- src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -3,6 +3,8 @@ package com.fastcampus.springbootpractice;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
 
@@ -10,9 +12,17 @@ import javax.annotation.PostConstruct;
 public class FastcampusSpringBootPracticeApplication {
 
     private final Integer height;
+    private final Environment environment;
+    private final ApplicationContext applicationContext;
 
-    public FastcampusSpringBootPracticeApplication(@Value("${my.height}") Integer height) {
+    public FastcampusSpringBootPracticeApplication(
+            @Value("${my.height}") Integer height,
+            Environment environment,
+            ApplicationContext applicationContext
+            ) {
         this.height = height;
+        this.environment = environment;
+        this.applicationContext = applicationContext;
     }
 
     public static void main(String[] args) {
@@ -23,5 +33,7 @@ public class FastcampusSpringBootPracticeApplication {
     @PostConstruct
     public void init(){
         System.out.println("[@Value] " + height);
+        System.out.println("[Environment] " + environment.getProperty("my.height"));
+        System.out.println("[ApplicationContext] " + applicationContext.getEnvironment().getProperty("my.height"));
     }
 }
