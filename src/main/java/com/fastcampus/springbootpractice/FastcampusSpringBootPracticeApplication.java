d1c0f3165cb91831d27fb4f3b8ee9a64eda58ffc
luvi-kh <nalsengom@daum.net>
2024-07-06 21:47:56 +0900

    @PostConstruct to EventListner
    스프링 캐시는 스프링 컨테이너의 모든 싱글톤 빈이
    인스턴스화 된 이후에 동작하게끔 되어있으므로,
    `@PostConstruct` 시점에서 동작을 보장할 수 없음
    
    스프링 부트 startup 이 끝난 직후 실행되는 다른 로직이 필요
    
    이에 `@EventListner` + `ApplicationReadyEvent`
    를 이용해 해결
    
    앞으로 다른 모든 기본 기능 관찰도 여기서 하면 될 듯

--- src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -6,11 +6,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 
 import javax.annotation.PostConstruct;
@@ -28,8 +30,11 @@ public class FastcampusSpringBootPracticeApplication {
         SpringApplication.run(FastcampusSpringBootPracticeApplication.class, args);
 
     }
-    
-    @PostConstruct
+    //Spring Cache는 모든 Bean들이 load된뒤에 활성화함
+    //PostConstruct 해당 클래스의 의존성이 모두 완성된 뒤임
+    //MyProperties, StudentService 로드했다고 해서, spring bean의 모든 Beam 이 load된 것이 아님
+    //@PostConstruct
+    @EventListener(ApplicationReadyEvent.class)
     public void init(){
 //        System.out.println("[configurationProps] " + myProperties.getHeight());
         studentService.printStudent("jack");
