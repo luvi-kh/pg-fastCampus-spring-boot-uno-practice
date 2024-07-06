d58fa681afdc5c688a3721551192963e48c21630
luvi-kh <nalsengom@daum.net>
2024-07-06 15:51:49 +0900

    PostConstruct 로 주입시기 오류문제 해결
    PostConstruct로 필드주입시 오류 해결

--- src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class FastcampusSpringBootPracticeApplication {
 
@@ -13,13 +15,10 @@ public class FastcampusSpringBootPracticeApplication {
     public static void main(String[] args) {
         SpringApplication.run(FastcampusSpringBootPracticeApplication.class, args);
 
-        FastcampusSpringBootPracticeApplication app = new FastcampusSpringBootPracticeApplication();
-        app.abc();
     }
     
-    //생성자주입이 아니라, 필드주입방식이기에, 인스턴스가 생성된 뒤에 주입되서 null이 나옴
-    public void abc() {
+    @PostConstruct
+    public void init(){
         System.out.println("[@Value] " + height);
     }
-
 }
