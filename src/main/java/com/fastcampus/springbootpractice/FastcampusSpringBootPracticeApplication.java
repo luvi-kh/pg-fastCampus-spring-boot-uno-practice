7a7955ade7c9cc53ca7b46ac412ec20f903da80d
luvi-kh <nalsengom@daum.net>
2024-07-06 15:48:56 +0900

    필드주입시 오류 예시
    필드주입시 값세팅이 안되는 오류 예시

--- src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
+++ src/main/java/com/fastcampus/springbootpractice/FastcampusSpringBootPracticeApplication.java
@@ -1,13 +1,25 @@
 package com.fastcampus.springbootpractice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FastcampusSpringBootPracticeApplication {
 
+    @Value("${my.height}")
+    private Integer height;
+
     public static void main(String[] args) {
         SpringApplication.run(FastcampusSpringBootPracticeApplication.class, args);
+
+        FastcampusSpringBootPracticeApplication app = new FastcampusSpringBootPracticeApplication();
+        app.abc();
+    }
+    
+    //생성자주입이 아니라, 필드주입방식이기에, 인스턴스가 생성된 뒤에 주입되서 null이 나옴
+    public void abc() {
+        System.out.println("[@Value] " + height);
     }
 
 }
